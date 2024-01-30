59516f6a6d4f1a8137b6420ea3a7a2c66e18dbc3
Davide Lodde <johndoe@example.com>
2024-01-30 18:38:42 +0100

    aggiornamento hero

--- js/hero.js
+++ js/hero.js
@@ -1,17 +1,56 @@
 const sliderHero = document.querySelector('.slider-hero');
-  const slides = document.querySelectorAll('.slide-hero');
-  let counter = 0;
+const slides = document.querySelectorAll('.slide-hero');
+const position1 = document.querySelector('.position1')
+const position2 = document.querySelector('.position2')
+const position3 = document.querySelector('.position3')
+const position4 = document.querySelector('.position4')
+const position5 = document.querySelector('.position5')
+let counter = 0;
 
-  setInterval(() => {
-    sliderHero.style.transition = 'transform 0.5s ease-in-out';
-    sliderHero.style.transform = 'translateX(' + (-counter * 100) + '%)';
-    counter++;
+setInterval(() => {
+  sliderHero.style.transition = 'none'; // Disabilita la transizione
+  sliderHero.style.transform = 'translateX(' + (-counter * 100) + '%)';
+  counter++;
 
-    if (counter === slides.length) {
-      setTimeout(() => {
-        sliderHero.style.transition = 'none';
-        sliderHero.style.transform = 'translateX(0)';
-        counter = 0;
-      }, 500);
-    }
-  }, 3000);
+
+  if (counter === slides.length) {
+    setTimeout(() => {
+      sliderHero.style.transform = 'translateX(0)';
+      counter = 0;
+
+      sliderHero.style.transition = 'none'; // Riabilita la transizione
+    }, 500);
+  }
+  console.log(counter);
+  switch (counter) {
+    case 1:
+      console.log("caso 1")
+      position1.style.display = 'block';
+      
+      break;
+    case 2:
+      console.log("caso 2")
+      position1.style.display = 'block';
+      position1.style.marginLeft = '22px';
+      
+      break;
+    case 3:
+      console.log("caso 3")
+      position1.style.display = 'block';
+      position1.style.marginLeft = '44px';
+      break;
+    case 4:
+      console.log("caso 4")
+      position1.style.display = 'block';
+      position1.style.marginLeft = '66px';
+      break;
+    case 5:
+      console.log("caso 5")
+      position1.style.display = 'block';
+      position1.style.marginLeft = '88px';
+      break;
+
+    default:
+      break;
+  }
+}, 3000);
