dbb34804210efc46c071774463bff350ab2c1dfe
Davide Lodde <johndoe@example.com>
2024-02-13 11:49:51 +0100

    darkmode

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,12 +1,14 @@
-document.querySelector(".slider").addEventListener("click", darkMode); 
+document.querySelector(".darkmode").addEventListener("click", darkMode); 
 //punto all div .slider dicendo che all evento "click" si esegua la funzione 'darkMode'
-let lightMode=true; // dichiaro variabile globale booleana inizializzata a true perchè il sito starta in ligth mode 
+let lightMode = true; // dichiaro variabile globale booleana inizializzata a true perchè il sito starta in ligth mode 
 function darkMode () { //funzione che mi cambia l'attributo 'data-theme' in dark e viceversa
   if(lightMode){
     document.documentElement.setAttribute('data-theme', 'dark');
     lightMode = false //diventando dark, ovviamente ligthMode diventa false
+    console.log('to dark')
   }else{
     document.documentElement.setAttribute('data-theme', 'light');
-    lightMode = true; //diventando ligth, ovviamente ligthMode diventa true
+    lightMode = true; //diventando light, ovviamente ligthMode diventa true
+    console.log('to light')
   }
 }
