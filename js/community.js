93140fad4cf30d717e2929f8be2e90ced96afdf4
martinamancuso <martinamancuso24@gmail.com>
2024-02-23 23:37:45 +0100

    addEventListener added to tweetBar and blogBar

--- js/community.js
+++ js/community.js
@@ -1,26 +1,48 @@
-document.getElementById("community-tweet").style.opacity = 1;
-document.getElementById("community-tweet").style.zIndex = 1;
-document.getElementById("community-blog").style.opacity = 0;
-document.getElementById("community-blog").style.zIndex = 0;
-document.getElementById("community-tweet-bar").classList.add("active");
+const tweet = document.getElementById("community-tweet");
+const tweetBar = document.getElementById("community-tweet-bar");
+const blog = document.getElementById("community-blog");
+const blogBar = document.getElementById("community-blog-bar");
+
+tweet.style.opacity = 1;
+tweet.style.zIndex = 1;
+blog.style.opacity = 0;
+blog.style.zIndex = 0;
+tweetBar.classList.add("active");
 
 let tweetVisible = true;
 
+function showTweet() {
+  tweet.style.opacity = 1;
+  tweet.style.zIndex = 1;
+  blog.style.opacity = 0;
+  blog.style.zIndex = 0;
+  tweetVisible = true;
+  tweetBar.classList.toggle("active");
+  blogBar.classList.toggle("active");
+}
+
+function showBlog() {
+  tweet.style.opacity = 0;
+  tweet.style.zIndex = 0;
+  blog.style.opacity = 1;
+  blog.style.zIndex = 1;
+  tweetVisible = false;
+  tweetBar.classList.toggle("active");
+  blogBar.classList.toggle("active");
+}
+
 setInterval(() => {
   if (tweetVisible) {
-    document.getElementById("community-tweet").style.opacity = 0;
-    document.getElementById("community-tweet").style.zIndex = 0;
-    document.getElementById("community-blog").style.opacity = 1;
-    document.getElementById("community-blog").style.zIndex = 1;
-    tweetVisible = false;
+    showBlog()
   } else {
-    document.getElementById("community-tweet").style.opacity = 1;
-    document.getElementById("community-tweet").style.zIndex = 1;
-    document.getElementById("community-blog").style.opacity = 0;
-    document.getElementById("community-blog").style.zIndex = 0;
-    tweetVisible = true;
+    showTweet()
   }
-  document.getElementById("community-tweet-bar").classList.toggle("active");
-  document.getElementById("community-blog-bar").classList.toggle("active");
 }, 5000)
 
+tweetBar.addEventListener("click", () => {
+  showTweet()
+})
+
+blogBar.addEventListener("click", () => {
+  showBlog()
+})
