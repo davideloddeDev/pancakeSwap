376ca80f1044fa3ce71a1847bfcf4400a5828615
martinamancuso <martinamancuso24@gmail.com>
2024-02-23 23:30:04 +0100

    added z-index to community.js

--- js/community.js
+++ js/community.js
@@ -1,5 +1,7 @@
 document.getElementById("community-tweet").style.opacity = 1;
+document.getElementById("community-tweet").style.zIndex = 1;
 document.getElementById("community-blog").style.opacity = 0;
+document.getElementById("community-blog").style.zIndex = 0;
 document.getElementById("community-tweet-bar").classList.add("active");
 
 let tweetVisible = true;
@@ -7,13 +9,18 @@ let tweetVisible = true;
 setInterval(() => {
   if (tweetVisible) {
     document.getElementById("community-tweet").style.opacity = 0;
+    document.getElementById("community-tweet").style.zIndex = 0;
     document.getElementById("community-blog").style.opacity = 1;
+    document.getElementById("community-blog").style.zIndex = 1;
     tweetVisible = false;
   } else {
     document.getElementById("community-tweet").style.opacity = 1;
+    document.getElementById("community-tweet").style.zIndex = 1;
     document.getElementById("community-blog").style.opacity = 0;
+    document.getElementById("community-blog").style.zIndex = 0;
     tweetVisible = true;
   }
   document.getElementById("community-tweet-bar").classList.toggle("active");
   document.getElementById("community-blog-bar").classList.toggle("active");
 }, 5000)
+
