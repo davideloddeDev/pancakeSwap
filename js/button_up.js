0ba7f681c64df68cdf552d4c462dd481d340c740
Alex <al.vaccaro@outlook.com>
2024-02-26 11:36:44 +0100

    fix button up: Adesso può essere visualizzato solo allo scroll della pgina

--- js/button_up.js
+++ js/button_up.js
@@ -1,8 +1,23 @@
-function scrollToTop() {
-    // Scorri fino alla cima della pagina
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth" // Effetto di scorrimento fluido
-    });
-    document.querySelector('.button-up').classList.add('clicked');
+// Get the button:
+let mybutton = document.getElementById("btn-up");
+
+// Quando l'utente scrolla oltre gli 800 px il pulsante scompare
+window.onscroll = function() {scrollFunction()};
+mybutton.style.display = "none"; // inizializzo il pulsante a none
+
+function scrollFunction() {  
+  
+  if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
   }
+}
+
+// al click scrolla "dolcemente" fino a top pagina
+function topFunction() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth" // Effetto di scorrimento fluido
+  });
+}
