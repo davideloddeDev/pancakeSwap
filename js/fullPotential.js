4d73f6a64721dd5b05d14d0ba9cbd5325c6a5463
Davide Lodde <johndoe@example.com>
2024-02-26 11:52:00 +0100

    full potential

---
+++ js/fullPotential.js
@@ -0,0 +1,42 @@
+function changeVideo() {
+    console.log(lightMode);
+  
+    let coinLightMedia = document.querySelector(".coin-light-mq");
+    let coinDarkMedia = document.querySelector(".coin-dark-mq");
+  
+    let coinLight = document.querySelector(".coin-light");
+    let coinDark = document.querySelector(".coin-dark");
+    
+    if (!lightMode) {
+      console.log('Showing dark mode videos');
+      coinDark.style.display = 'block';
+      coinLight.style.display = 'none';
+    } else {
+      console.log('Showing light mode videos');
+      coinDark.style.display = 'none';
+      coinLight.style.display = 'block';
+    }
+  }
+  
+  
+  function checkMediaQuery() {
+    let coinLightMedia = document.querySelector(".coin-light-mq");
+    let coinDarkMedia = document.querySelector(".coin-dark-mq");
+  
+    if (window.matchMedia("(max-width: 967px)").matches) {
+      // Media query attiva, imposta il display dei video in base al tema corrente
+      if (!lightMode) {
+        coinDarkMedia.style.display = 'block';
+        coinLightMedia.style.display = 'none';
+      } else {
+        coinDarkMedia.style.display = 'none';
+        coinLightMedia.style.display = 'block';
+      }
+    } else {
+      changeVideo();
+    }
+  }
+    
+    window.matchMedia("(max-width: 967px)").addEventListener(checkMediaQuery);
+    
+    checkMediaQuery();
